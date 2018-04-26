b9d6e1dea2f4b69e2977e55a9e7595748e3a666f
LinkeZzz <linkegilmanova@gmail.com>
2018-04-26 15:43:43 +0300

    backup for canvas

--- radio/static/js/file1.js
+++ radio/static/js/file1.js
@@ -12,6 +12,35 @@ function main() {
   //init_gl(canvas);
 
   const ctx = canvas.getContext('2d');
+  var history = {
+      redo_list: [],
+      undo_list: [],
+      saveState: function(canvas, list, keep_redo){
+          keep_redo=keep_redo||false;
+          if(!keep_redo){
+              this.redo_list=[];
+          }
+          (list||this.undo_list).push(canvas.toDataURL());
+      },
+      undo: function(canvas, ctx) {
+          this.restoreState(canvas, ctx, this.undo_list, this.redo_list);
+      },
+      redo: function(canvas, ctx) {
+          this.restoreState(canvas, ctx, this.redo_list, this.undo_list);
+      },
+      restoreState:function (canvas, ctx, pop, push) {
+          if(pop.length){
+              this.saveState(canvas, push, true);
+              var restore_state = pop.pop();
+              var img = new Element('img',{'src':restore_state});
+              img.onload = function () {
+                  ctx.clearRect(0,0,c_H, c_W);
+                  ctx.drawImage(img, 0,0, c_H, c_W,0,0,c_H, c_W);
+              }
+          }
+      }
+      //--
+  };
   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
   // Sliders Action
@@ -25,7 +54,7 @@ function main() {
   range_x.onchange=function(){
     $.ajax({
         type:"GET",
-        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:0},
+        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:0,draw:1},
         dataType:'json',
         success:function(json){
             var id = ctx.createImageData(1,1); // only do this once per page
@@ -51,7 +80,7 @@ function main() {
   range_y.onchange=function(){
       $.ajax({
         type:"GET",
-        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:1},
+        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:1,draw:1},
         dataType:'json',
         success:function(json){
             var id = ctx.createImageData(1,1); // only do this once per page
@@ -77,7 +106,7 @@ function main() {
   range_z.onchange= function(){
       $.ajax({
         type:"GET",
-        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:2},
+        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:2, draw:1},
         dataType:'json',
         success:function(json){
             var id = ctx.createImageData(1,1); // only do this once per page
@@ -100,6 +129,41 @@ function main() {
           console.log('error');
         }});
   }
+
+  // Buttons Action CT
+  var buttonsCT = document.getElementsByClassName('btn-ct');
+  for(var i=0;i<buttonsCT.length();i++)
+      buttonsCT[i].onclick=function(){
+      alert("BUTTON CLICKED CSV download ...");
+      $.ajax({
+          type:"GET",
+          data:{get_csv: 1},
+          dataType:'json',
+        success:function(json){
+            //TODO: print in list
+        },
+        error: function(error, txtStatus){
+          console.log(txtStatus);
+          console.log('error');
+        }});
+
+      };
+}
+
+function dump_info(){
+    //TODO: get info from list element under predicted
+    console.log("DUMP CALLED");
+    $.ajax({
+        type:"POST",
+        data: json,
+        success: function (){
+            alert("CSV files suxessfully is creted");
+        },
+        error: function(error, txtStatus){
+          console.log(txtStatus);
+          console.log('error');
+        }
+    });
 }
 
 function init_gl(canvas){
@@ -107,12 +171,13 @@ function init_gl(canvas){
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
+
 function predictFunction(){
     draw_circle(170,170,30);
 }
-function draw_circle(x, y, r){
-    const canvas = document.querySelector("#glCanvas");
-    const ctx = canvas.getContext('2d');
+
+function draw_circle(ctx, x, y, r){
+    
     ctx.strokeStyle="#FF0000";
     ctx.lineWidth=2;
     ctx.beginPath();
@@ -120,10 +185,10 @@ function draw_circle(x, y, r){
     ctx.stroke();
 }
 
-function draw_retangle(){
-
+function ct_annotation_func() {
+    //TODO draw on canvas everything from list1
 }
-function dump(){
-    console.log("DUMP CALLED");
 
+function predict_func(){
+    //TODO: draw on canvas everything from list2
 }
