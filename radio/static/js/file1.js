69acf1098640f88ef6ebc571bb690af0d345768c
LinkeZzz <linkegilmanova@gmail.com>
2018-04-26 10:14:01 +0300

    form

--- radio/static/js/file1.js
+++ radio/static/js/file1.js
@@ -1,64 +1,129 @@
 main();
 
-//
-// start here
-//
 function main() {
 
-  $.ajax('',{
-    data:{
-      id:'image'
-    }
-  }).then(
-      function success(){alert("Image data successfuly send");},
-      function fail(data, status){alert("Request failed. Returned status of"+status);}
-  )
+  console.log("logging works!");
   const canvas = document.querySelector("#glCanvas");
-  console.log(typeof canvas);
-  var image3d = document.querySelector("#imgCT");
-  console.log(typeof image3d);
-  for (var key in image3d){
-    console.log(key);
+  //TODO:
+  var c_W=550;
+  var c_H=550;
+  canvas.width  = c_W;
+  canvas.height = c_H;
+  //init_gl(canvas);
+
+  const ctx = canvas.getContext('2d');
+  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  // Sliders Action
+  var range_x = document.getElementById("myRange1");
+  var range_y = document.getElementById("myRange2");
+  var range_z = document.getElementById("myRange3");
+
+  var g_width=512;
+  var g_height=512;
+
+  range_x.onchange=function(){
+    $.ajax({
+        type:"GET",
+        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:0},
+        dataType:'json',
+        success:function(json){
+            var id = ctx.createImageData(1,1); // only do this once per page
+            var d  = id.data;
+            var i,j;
+            //TODO:
+            for(i=0;i<g_height;i++){
+                for(j=0;j<g_width;j++){
+                    var tmp = json[i][j];
+                    d[3]   = 255;//tmp & 0xFF;
+                    d[0]   = tmp;//(tmp & 0xFF00) >>> 8;
+                    d[1]   = tmp;//(tmp & 0xFF0000) >>> 16;
+                    d[2]   = tmp;//(tmp & 0xFF000000) >>> 24; // <<24-alpha <<16 blue <<8 green red
+                    ctx.putImageData( id, i, j );
+                }
+            }
+        },
+        error: function(error, txtStatus){
+          console.log(txtStatus);
+          console.log('error');
+        }});
+  };
+  range_y.onchange=function(){
+      $.ajax({
+        type:"GET",
+        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:1},
+        dataType:'json',
+        success:function(json){
+            var id = ctx.createImageData(1,1); // only do this once per page
+            var d  = id.data;
+            var i,j;
+            //TODO:
+            for(i=0;i<g_height;i++){
+                for(j=0;j<g_width;j++){
+                    var tmp = json[i][j];
+                    d[3]   = 255;//tmp & 0xFF;
+                    d[0]   = tmp;//(tmp & 0xFF00) >>> 8;
+                    d[1]   = tmp;//(tmp & 0xFF0000) >>> 16;
+                    d[2]   = tmp;//(tmp & 0xFF000000) >>> 24; // <<24-alpha <<16 blue <<8 green red
+                    ctx.putImageData( id, i, j );
+                }
+            }
+        },
+        error: function(error, txtStatus){
+          console.log(txtStatus);
+          console.log('error');
+        }});
+  };
+  range_z.onchange= function(){
+      $.ajax({
+        type:"GET",
+        data:{x: range_x.value, y: range_y.value, z: range_z.value, axis:2},
+        dataType:'json',
+        success:function(json){
+            var id = ctx.createImageData(1,1); // only do this once per page
+            var d  = id.data;
+            var i,j;
+            //TODO:
+            for(i=0;i<g_height;i++){
+                for(j=0;j<g_width;j++){
+                    var tmp = json[i][j];
+                    d[3]   = 255;//tmp & 0xFF;
+                    d[0]   = tmp;//(tmp & 0xFF00) >>> 8;
+                    d[1]   = tmp;//(tmp & 0xFF0000) >>> 16;
+                    d[2]   = tmp;//(tmp & 0xFF000000) >>> 24; // <<24-alpha <<16 blue <<8 green red
+                    ctx.putImageData( id, i, j );
+                }
+            }
+        },
+        error: function(error, txtStatus){
+          console.log(txtStatus);
+          console.log('error');
+        }});
   }
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
-
-  // Only continue if WebGL is available and working
-  if (!gl) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-    return;
-  }
-  // set canvas by float 32 arr
-
-
-  // Set clear color to black, fully opaque
-    // Clear the color buffer with specified clear color
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
 }
-function draw_image(){
 
-  var vertexShaderSource=`
-  attribute vec4_a_position;
-  attribute vec4 a_texcoord;
-  
-  uniform mat4 u_matrix;
-  varying vec2 v_texcoord;
-  
-  void main(){
-    gl_Position = u_matrix*a_position;
-    }
-  `;
-  var fragmentShaderSource = `
-  precision mediump float;
-  varying vec2 v_textcoord;
-  
-  uniform sampler2D ;
-  varying vec2 v_texcoord;
-  
-  void main(){
-    gl_Position = u_matrix*a_position;
-    }
-  `;
+function init_gl(canvas){
+    const gl = canvas.getContext("webgl");
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+function predictFunction(){
+    draw_circle(170,170,30);
+}
+function draw_circle(x, y, r){
+    const canvas = document.querySelector("#glCanvas");
+    const ctx = canvas.getContext('2d');
+    ctx.strokeStyle="#FF0000";
+    ctx.lineWidth=2;
+    ctx.beginPath();
+    ctx.arc(x,y,r,0, Math.PI*2, true);
+    ctx.stroke();
+}
+
+function draw_retangle(){
+
+}
+function dump(){
+    console.log("DUMP CALLED");
 
 }
--- radio/views.py
+++ radio/views.py
@@ -1,37 +1,73 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.http import StreamingHttpResponse
+from django.core import serializers
+from django.http import HttpResponse
+from json import JSONEncoder
+from django.http import JsonResponse
+from django.template import Context, Template
+
+import datetime
+import blosc
 import os
 import mimetypes
-from django.http import StreamingHttpResponse
 from wsgiref.util import FileWrapper
+from json import JSONEncoder
+import codecs
+import json
+import numpy as np
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
 
-
+def read_blosc(file):
+    with open(file, mode='rb')as file:
+        packed = file.read()
+    array = blosc.unpack_array(packed)
+    return array
 
 def ct_image(request):
     return render(request, 'radio/index.html', {})
 
-def get_ct_image(request, id=0):
-    #TODO: add id-path relationship
-    file_path="C:\data.blk"
-    filename = os.path.basename(file_path)
-    chunk_size = 8192
-    response = StreamingHttpResponse(FileWrapper(open(file_path,'rb'), chunk_size),
-                                     content_type=mimetypes.guess_type(file_path)[0])
-    response['Content-Length'] = os.path.getsize(file_path)
-    response['Content-Disposition'] = "attachment; filename=%s" % filename
+def get_ct_image(request):
+
+    file_path = "/home/linke/Desktop/web_radio/radio/static/data.blk"
+
+    array = read_blosc(file_path)
+
+    if request.method == 'GET' and request.is_ajax():
+        x = int(request.GET.get('x'))
+        y = int(request.GET.get('y'))
+        z = int(request.GET.get('z'))
+        s=x
+
+        if (int(request.GET.get('axis'))==1):
+            array = np.swapaxes(array, 0, 1)#ok
+            s = y
+
+        if (int(request.GET.get('axis'))==2):
+            array = np.swapaxes(array, 0, 2)#ok
+            s=z
+
+        array = array[s]
+
+        array = array.tolist()
+        d = {}
+        i = 0
+        for x in array:
+            d[i]=x
+            i+=1
+        out = json.dumps(d)
+        return HttpResponse(out)
+
     return render(request, 'radio/index.html', {})
 
 def get_ecg_image(request):
-    pass
 
-def get_csv_file(request):
-    # TODO: add id-path relationship
-    file_path="C:\data.blk"
-    filename = os.path.basename(file_path)
-    chunk_size = 8192
-    response = StreamingHttpResponse(FileWrapper(open(file_path,'rb'), chunk_size),
-                                     content_type=mimetypes.guess_type(file_path)[0])
-    response['Content-Length'] = os.path.getsize(file_path)
-    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    response = HttpResponse(content_type='application/jpeg')
+    response['Content-Disposition'] = 'filename="somefilename.pdf"'
+    p = canvas.Canvas(response)
+    p.drawString(100, 100, "Hello world")
+    p.showPage()
+    p.save()
     return response
